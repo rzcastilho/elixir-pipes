8e55761961e13a8b989f6e01a9bdc8acece4340c
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2019-10-02 00:27:59 -0300

    Last version

--- lib/elixir_pipes.ex
+++ lib/elixir_pipes.ex
@@ -1,13 +1,24 @@
 defmodule ElixirPipes do
+
+  def valid_line([_, _]), do: true
+  def valid_line(_), do: false
+
+  def call_api([id, _]) do
+    HTTPoison.get!("https://api.punkapi.com/v2/beers/#{id}")
+  end
+
+  def filter_success(%{status_code: 200}), do: true
+  def filter_success(_), do: false
+
   def pipe do
     File.read!("beers.csv")
-    |> String.trim()
     |> String.split("\n")
     |> Enum.map(fn line -> String.split(line, ",") end)
-    |> Enum.filter(fn values -> Enum.count(values) == 2 end)
-    |> Enum.map(fn [id, _] -> HTTPoison.get!("https://api.punkapi.com/v2/beers/#{id}") end)
-    |> Enum.filter(fn %{status_code: status_code} -> status_code == 200 end)
+    |> Enum.filter(&valid_line/1)
+    |> Enum.map(&call_api/1)
+    |> Enum.filter(&filter_success/1)
     |> Enum.map(fn %{body: body} -> Jason.decode!(body, keys: :atoms) end)
     |> Enum.map(fn [%{id: id, name: name, abv: abv}] -> %{id: id, name: name, abv: abv} end)
   end
+
 end
